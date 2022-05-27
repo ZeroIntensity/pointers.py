19bebbc831e0c3eaace745a2216d7ee7b5490db6
benefactarch <auch.josh@gmail.com>
2022-05-26 22:33:12 -0400

    Update _cstd.py

--- src/pointers/_cstd.py
+++ src/pointers/_cstd.py
@@ -6,7 +6,7 @@ __all__ = ("c_malloc", "c_free", "c_realloc", "c_calloc")
 dll = ctypes.CDLL(
     "msvcrt" if os.name == "nt" else "libc.dylib"
     if os.name == "posix" else "libc.so.6"
-) # mac = posix, linux = so.6, windows = msvcrt
+) # mac = dylib, linux = so.6, windows = msvcrt
 
 # void* malloc(size_t size);
 dll.malloc.argtypes = (ctypes.c_size_t,)
