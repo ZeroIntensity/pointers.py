e1ad9b72d1d3b198b4b5a82054ea0a2a4b34e233
ZeroIntensity <zintensitydev@gmail.com>
2022-08-08 00:06:18 -0400

    add some of #21

--- src/pointers/__init__.py
+++ src/pointers/__init__.py
@@ -1,32 +1,17 @@
-from .pointer import Pointer, to_ptr, dereference_address, force_set_attr, _
-from .malloc import (
-    malloc,
-    IsMallocPointerError,
-    MallocPointer,
-    free,
-    realloc,
-)
-from .exceptions import (
-    AllocationError,
-    IsMallocPointerError,
-    NotEnoughChunks,
-    IsFrozenError,
-    DereferenceError,
-    FreedMemoryError,
-    InvalidSizeError,
-    InvalidBindingParameter,
-)
-from .calloc import calloc, CallocPointer
-from .frozen_pointer import to_const_ptr, FrozenPointer
-from .decay import decay
-from .c_pointer import (
-    TypedCPointer,
-    VoidPointer,
-    cast,
-    StructPointer,
-    to_c_ptr,
-    to_struct_ptr,
-)
-from .struct import Struct
 from .bindings import *
-from .custom_binding import binds, binding
+from .c_pointer import (
+    StructPointer, TypedCPointer, VoidPointer, cast, to_c_ptr, to_struct_ptr
+)
+from .calloc import CallocPointer, calloc
+from .custom_binding import binding, binds
+from .decay import decay
+from .exceptions import (
+    AllocationError, DereferenceError, FreedMemoryError,
+    InvalidBindingParameter, InvalidSizeError, IsFrozenError,
+    IsMallocPointerError, NotEnoughChunks
+)
+from .frozen_pointer import FrozenPointer, to_const_ptr
+from .malloc import IsMallocPointerError, MallocPointer, free, malloc, realloc
+from .pointer import Pointer, _, dereference_address, force_set_attr, to_ptr
+from .struct import Struct
+from .version import __version__
