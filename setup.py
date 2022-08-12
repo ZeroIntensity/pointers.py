b26c51c642cbb42743b489ed6be867b1cef727c4
ZeroIntensity <zintensitydev@gmail.com>
2022-08-12 16:32:38 -0400

    i hate build systems

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 from setuptools import Extension, setup
 
+with open("./README.md") as f:
+    long_desc: str = f.read()
+
 if __name__ == "__main__":
     setup(
-        ext_modules=[Extension("_pointers", ["./src/mod.c"])],
+        name="pointers.py",
+        version="2.0.0",
+        author="ZeroIntensity",
+        author_email="<zintensitydev@gmail.com>",
+        description="Bringing the hell of pointers to Python.",
+        long_description_content_type="text/markdown",
+        long_description=long_desc,
+        packages=["pointers"],
+        keywords=["python", "pointers"],
+        install_requires=["typing_extensions"],
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: Implementation :: CPython",
+        ],
         license="MIT",
+        project_urls={
+            "Source": "https://github.com/ZeroIntensity/pointers.py",
+            "Documentation": "https://pointerspy.netlify.app/",
+        },
+        package_dir={"": "src"},
+        ext_modules=[Extension("_pointers", ["./src/mod.c"])],
     )
