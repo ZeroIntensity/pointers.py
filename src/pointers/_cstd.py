5ae2ac359bd238cf4326af6cd8aa130d8ebcb3d2
UltraStudioLTD <57063715+UltraStudioLTD@users.noreply.github.com>
2022-11-30 10:44:59 +0400

    Fix incorrect keyword

--- src/pointers/_cstd.py
+++ src/pointers/_cstd.py
@@ -24,7 +24,7 @@ else:
     _c_library_name = find_library("c") or "libc.so.6"
 
 dll = ctypes.CDLL(_c_library_name)
-mdll = dll if platform in ("win32", "cygwin") else ctypes.CDLL(find_library("m") else "libm.so.6")
+mdll = dll if platform in ("win32", "cygwin") else ctypes.CDLL(find_library("m") or "libm.so.6")
 
 class tm(ctypes.Structure):
     _fields_ = [
