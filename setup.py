fcaa2c4617a7d043d8ff395eac7702fa3e107dde
Peter <49501366+ZeroIntensity@users.noreply.github.com>
2024-04-01 12:29:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import Extension, setup
 
 if __name__ == "__main__":
     setup(
+        name="pointers.py",
         packages=["pointers"],
         package_dir={"": "src"},
         ext_modules=[Extension("_pointers", ["./src/mod.c"])],
